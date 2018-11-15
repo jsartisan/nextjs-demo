19c3d35ccd7a9f3d5b0bdd7ddc3bc147a68a1b76
Pawan Kumar <pawan.kumar@squareboat.com>
2018-11-15 22:04:52 +0530

    add sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import App, { Container } from 'next/app';
 
-import '../assets/css/style.css';
+import '../assets/sass/style.scss';
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
