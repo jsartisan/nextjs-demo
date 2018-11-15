007ee3bb5916eef8508f4340441cd168df85fb02
Pawan Kumar <pawan.kumar@squareboat.com>
2018-11-15 21:59:20 +0530

    query fix

--- pages/Post.js
+++ pages/Post.js
@@ -1,13 +1,19 @@
-import { Link } from '../routes';
+import { Link, Router } from '../routes';
 
-export default ({ url }) => (
+const Post =  ({ query }) => (
   <div>
-    <h1>{url.query.slug }</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias error voluptatibus assumenda soluta dolores, odit, ducimus doloribus libero quo in. Iusto omnis eveniet fuga harum soluta porro voluptate vel nihil.</p>
+    <h1>{query.slug}</h1>
     <ul>
       <li><Link route="home"><a>Home</a></Link></li>
-      <li><Link route="about"><a>About</a></Link></li>
+      <li><a onClick={() => Router.pushRoute('about')}>About</a></li>
       <li><Link route="posts" params={{ slug: 'best-post-ever'}}><a>Post</a></Link></li>
     </ul>
   </div>
 );
+
+Post.getInitialProps = async ({ query }) => {
+  return { query }
+};
+
+
+export default Post;
